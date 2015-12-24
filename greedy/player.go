37799badba5d5501a643cd307f2ca6056b142287
Andrew Maach <andrew.maach@gmail.com>
2015-12-24 16:20:22 -0600

    Main mechanics working

---
+++ greedy/player.go
@@ -0,0 +1,200 @@
+package greedy
+
+import (
+    "math/rand"
+    "fmt"
+)
+
+const FirstRoundMinScore int = 800
+const WinningScore int = 10000
+
+type Strategy struct {
+    Id string
+    MinScore int
+}
+
+func (s *Strategy) DoRollFirst(dice Dice, minScore int) bool {
+    return dice.RunningScore > s.MinScore
+}
+
+func (s *Strategy) DoRoll(dice Dice) bool {
+    return dice.RunningScore < s.MinScore
+}
+
+type Dice struct {
+    Numbers [8]int
+    Count int
+    RunningScore int
+}
+
+var FreshDice Dice = Dice {
+    Numbers: [8]int{0,0,0,0,0,0,0,0},
+    Count: 8,
+    RunningScore: 0,
+}
+
+type Player struct {
+    Score int
+    Plan Strategy
+}
+
+func CreateGame(strategies []Strategy) Game {
+    game := Game{[]*Player{}, false}
+    for _, strategy := range strategies {
+        game.Players = append(game.Players, &Player{
+            Score: 0,
+            Plan: strategy,
+        })
+    }
+    return game
+}
+
+type Game struct {
+    Players []*Player
+    LastRound bool
+}
+
+func (g *Game) HighestScore() int {
+    highest := 0
+    for _, player := range g.Players {
+        if player.Score > highest {
+            highest = player.Score
+        }
+    }
+    
+    return highest
+}
+
+func (g *Game) Play() {
+    turns := make(chan *Player, len(g.Players) + 1)
+    
+    for _, player := range g.Players {
+        turns <- player
+    }
+    
+    dice := FreshDice
+    
+    for player := range turns {
+        minScore := 0
+        
+        // First round minimum score.
+        if player.Score == 0 {
+            minScore = FirstRoundMinScore
+        }
+        
+        if g.LastRound {
+            minScore = g.HighestScore() - player.Score + 1
+        }
+        
+        dice = player.RunTurn(dice, minScore)
+        fmt.Printf("%s scored %d, totaling %d \n \n", player.Plan.Id, dice.RunningScore, player.Score)
+        
+
+        
+        if !g.LastRound {
+            if player.Score >= WinningScore {
+                g.LastRound = true
+                close(turns)
+            } else {
+                 turns <- player
+            }
+           
+        }
+    }
+}
+
+func (p *Player) RunTurn(passedDice Dice, minScore int) Dice {
+    dice := FreshDice
+    kept := false
+    if p.Plan.DoRollFirst(passedDice, minScore) {
+        dice = passedDice
+        fmt.Println("Keeping dice")
+        kept = true
+    }
+    
+
+    
+    for kept || p.Plan.DoRoll(dice) || dice.RunningScore < minScore {
+        if !dice.Roll() {
+            return FreshDice
+        }
+        kept = false
+    }
+    
+    p.Score += dice.RunningScore
+    
+    return dice
+}
+
+func (d *Dice) Roll() bool {
+    for i := 0; i < d.Count; i++ {
+        d.Numbers[i] = rand.Intn(6) + 1
+    }
+    
+    return d.evaluateScore()
+}
+
+func (d *Dice) evaluateScore() bool {
+    rollScore := 0
+    
+    type Evaluation struct {
+        Count [6]int
+    }
+    
+     fmt.Printf("rolled: ")
+    
+     // Count how many there are of each number.
+    var eval Evaluation
+    for i := 0; i < d.Count; i++ {
+        fmt.Printf("%d ", d.Numbers[i])
+        eval.Count[d.Numbers[i] - 1] += 1
+    }
+    
+    
+    
+    // Check for numbers with 3 or more.
+    for i := 0; i < 6; i++ {
+         if eval.Count[i] >= 3 {
+             eval.Count[i] -= 3 // Decrement evaluation numbers.
+             d.Count -= 3 // Decrement dice count.
+             if i == 0 {
+                 rollScore += 1000
+             } else {
+                 rollScore += (i + 1) * 100
+             }
+             i -= 1 // To catch two sets of the same number.
+         }
+         
+    }
+    
+    // Check for ones.
+     rollScore += eval.Count[0] * 100
+     d.Count -= eval.Count[0] // Decrement dice count.
+     
+    // Check for fives.
+     rollScore += eval.Count[4] * 50
+     d.Count -= eval.Count[4] // Decrement dice count.
+     
+     
+     if d.Count == 0 {
+         d.Count = 8
+     }
+     
+     fmt.Printf("(%d points, %d die left, total %d)\n", rollScore, d.Count, d.RunningScore)
+     
+     if rollScore == 0 {
+         d.RunningScore = 0
+     } else {
+         d.RunningScore += rollScore
+     }
+     return rollScore != 0
+     
+}
+
+func (d *Dice) Print() {
+    fmt.Printf("   dice left: %d, running score: %d, dice:", d.Count, d.RunningScore)
+    for i := 0; i < 8; i++ {
+        fmt.Printf("%d ", d.Numbers[i])
+    }
+    fmt.Println()
+}
--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+    "github.com/andrewmaach/GreedyAI/greedy"
+    "math/rand"
+    "time"
     "fmt"
 )
 
 func main() {
-    fmt.Println("Hello!")
-    hello()
+    rand.Seed(time.Now().UnixNano())
+    
+    for i := 0 ; i < 1 ; i++ {
+        strategySet := []greedy.Strategy{
+            greedy.Strategy{"R2-D2", 200},
+            greedy.Strategy{"BB-8", 400},
+        }
+        
+        game := greedy.CreateGame(strategySet)
+        
+        game.Play()
+        for _, player := range game.Players {
+            fmt.Printf("%s: %d\n", player.Plan.Id, player.Score)
+        }
+    }
+    
 }
